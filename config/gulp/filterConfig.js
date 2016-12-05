0452a994ebd849e7fb8b7367f7c34bd2f258320e
xinshangshangxin <xinshangshangxin@gmail.com>
2016-12-05 13:03:27 +0800

    chore: 修改lodash注入内容

--- config/gulp/filterConfig.js
+++ config/gulp/filterConfig.js
@@ -6,20 +6,37 @@ module.exports = function (fn) {
 `;
 
 const lodashStr = `{
-  Array: Array,
-  Date: Date,
-  Error: Error,
-  Function: Function,
-  Math: Math,
-  Object: Object,
-  RegExp: RegExp,
-  String: String,
-  TypeError: TypeError,
-  setTimeout: setTimeout,
-  clearTimeout: clearTimeout,
-  setInterval: setInterval,
-  clearInterval: clearInterval
-};`;
+    Array: Array,
+    // Buffer: Buffer,
+    DataView: DataView,
+    Date: Date,
+    Error: Error,
+    Float32Array: Float32Array,
+    Float64Array: Float64Array,
+    Function: Function,
+    Int16Array: Int16Array,
+    Int32Array: Int32Array,
+    Int8Array: Int8Array,
+    // Map: Map,
+    Math: Math,
+    Object: Object,
+    Promise: Promise,
+    RegExp: RegExp,
+    // Set: Set,
+    String: String,
+    // Symbol: Symbol,
+    TypeError: TypeError,
+    Uint16Array: Uint16Array,
+    Uint32Array: Uint32Array,
+    Uint8Array: Uint8Array,
+    Uint8ClampedArray: Uint8ClampedArray,
+    WeakMap: WeakMap,
+    // _: _,
+    clearTimeout: clearTimeout,
+    isFinite: isFinite,
+    parseInt: parseInt,
+    setTimeout: setTimeout,
+  };`;
 
 const obj = {
   npmModules: [{
@@ -30,8 +47,7 @@ const obj = {
     src: ['lodash/lodash.js'],
     subStr: /var root = freeGlobal.*/,
     newStr: `var root = ${lodashStr}`,
-  }]
+  }],
 };
-
 
 module.exports = obj;
